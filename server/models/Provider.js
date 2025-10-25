85bb23521bb9705af9b526ed2a0cfcebf6f7d32a
Hạnh Meii <nguynqantan@gmail.com>
2025-10-25 16:02:50 +0700

    Add 'dev' script to run client and server at the same time

--- server/models/Provider.js
+++ server/models/Provider.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const providerSchema = new mongoose.Schema({
   //mongoose.Schema.Types.ObjectId là một kiểu dữ liệu đặc biệt dùng để lưu ID của tài liệu (document) khác trong MongoDB
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  cccd: { type: String, required: true },
-  ngayCapCCCD: { type: Date, required: true },
-  noiCapCCCD: { type: String, required: true },
-  soTaiKhoanNganHang: { type: String, required: true },
-  tenNganHang: { type: String, required: true },
-  cccdTruoc: { type: String, required: true },
-  cccdSau: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  cccd: { type: String },
+  ngayCapCCCD: { type: Date },
+  noiCapCCCD: { type: String },
+  soTaiKhoanNganHang: { type: String },
+  tenNganHang: { type: String },
+  cccdTruoc: { type: String },
+  cccdSau: { type: String },
 });
 
 module.exports = mongoose.model("Provider", providerSchema);
